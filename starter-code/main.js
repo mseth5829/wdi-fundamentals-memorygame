0e02d4ba5fe75fe4f02bfaa3cee60c1cf7f574f2
Mikella Seth <mikellaseth@Mikellas-MacBook-Pro.local>
2016-08-02 16:39:18 +0800

    added if/else statements

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,12 @@
-console.log("JS file is connected to HTML! Woo!")
+// Create card variables
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+if (cardOne == cardThree) {
+	alert("You've found a match!")
+} else {
+	alert("Sorry, better luck next time!")
+}
