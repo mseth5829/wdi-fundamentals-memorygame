b003ace0441342bc4d95be0e21433c2b8f0cb661
Mikella Seth <mikellaseth@Mikellas-MacBook-Pro.local>
2016-08-02 17:50:14 +0800

    for/while loop functionality created for card board

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,24 @@
-// Create card variables
+// Create card and board variables
 
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+var gameBoard = document.getElementById("game-board");
 
 if (cardOne == cardThree) {
 	alert("You've found a match!")
 } else {
 	alert("Sorry, better luck next time!")
 }
+
+
+function createBoard () {
+	for(var i=0;i<4;i+=1 ){
+		var createCard = document.createElement("div");
+		createCard.className = "card";
+		gameBoard.appendChild(createCard);
+	}
+}
+
+createBoard();
